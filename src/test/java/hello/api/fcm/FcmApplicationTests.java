e0eaccdf849b9e9d0f304f1a69dfd42e8f8b3dbe
Codeliner <codeliner96@gmail.com>
2021-06-02 14:42:57 +0900

    Created a new project.

---
+++ src/test/java/hello/api/fcm/FcmApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.api.fcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
